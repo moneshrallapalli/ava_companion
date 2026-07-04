5f60dbf79d9d2647d25fe3faa36e418e63070c00
Monesh Rallapalli <moneshrallapalli@gmail.com>
2026-07-04 17:27:32 -0400

    Refactor graph construction to use conversation_node instead of echo_node

--- src/ai_companion/lab/graph.py
+++ src/ai_companion/lab/graph.py
@@ -2,13 +2,15 @@ from langgraph.graph import StateGraph, START, END
 
 from langchain_core.messages import AIMessage
 from ai_companion.lab.state import LabState
+from ai_companion.lab.nodes import conversation_node
+
 
 def echo_node(state:  LabState):
     last = state["messages"][-1]
     return {"messages":[AIMessage(content=f"echo:{last.content}")]}
 
 graph_builder = StateGraph(LabState)
-graph_builder.add_node("echo_node", echo_node)
-graph_builder.add_edge(START, "echo_node")
-graph_builder.add_edge("echo_node", END)
+graph_builder.add_node("conversational_node", conversation_node)
+graph_builder.add_edge(START, "conversational_node")
+graph_builder.add_edge("conversational_node", END)
 graph = graph_builder.compile()
