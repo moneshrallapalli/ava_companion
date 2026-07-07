a2014402f328e0e42bb844c963f7c21b4f9955d6
Monesh Rallapalli <moneshrallapalli@gmail.com>
2026-07-07 00:53:15 -0400

    Add image and audio nodes; update workflow selection logic in graph builder

--- src/ai_companion/lab/graph.py
+++ src/ai_companion/lab/graph.py
@@ -2,17 +2,49 @@ from langgraph.graph import StateGraph, START, END
 
 from langchain_core.messages import AIMessage
 from ai_companion.lab.state import LabState
-from ai_companion.lab.nodes import conversation_node, context_injection_node
+from ai_companion.lab.nodes import conversation_node, context_injection_node, router_node
 
 
 # def echo_node(state:  LabState):
 # last = state["messages"][-1]
 # return {"messages":[AIMessage(content=f"echo:{last.content}")]}
+#just for now. Will be replaced with actual node.
+def image_node(state):
+    return{}
+
+def audio_node(state):
+    return{}
+
+
+def select_workflow(state):
+    if state["workflow"] == "image":
+        return "image_node"
+    elif state["workflow"] == "audio":
+        return "audio_node"
+    else:
+        return "conversation_node"
+
+
+
 
 graph_builder = StateGraph(LabState)
-graph_builder.add_node("conversational_node", conversation_node)
+graph_builder.add_node("conversation_node", conversation_node)
 graph_builder.add_node("context_injection_node", context_injection_node)
-graph_builder.add_edge(START, "context_injection_node")
-graph_builder.add_edge("context_injection_node","conversational_node")
-graph_builder.add_edge("conversational_node", END)
+graph_builder.add_node("image_node", image_node)
+graph_builder.add_node("audio_node", audio_node)
+
+graph_builder.add_node("router_node", router_node)
+
+#graph_builder.add_edge(START, "context_injection_node")
+graph_builder.add_edge(START, "router_node")
+graph_builder.add_conditional_edges("router_node", select_workflow)
+#graph_builder.add_edge("context_injection_node","conversation_node")
+graph_builder.add_edge("image_node", END)
+graph_builder.add_edge("audio_node", END)
+graph_builder.add_edge("conversation_node", END)
+#graph_builder.add_edge("conversation_node", END)
 graph = graph_builder.compile()
+
+
+
+
