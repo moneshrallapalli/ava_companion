331db9c7b6549d58c281d2df91187b61fbc54b53
Monesh Rallapalli <moneshrallapalli@gmail.com>
2026-07-08 22:04:40 -0400

    Initialize ChatGroq in MemoryManager for structured output handling

--- src/ai_companion/lab/memory_manager.py
+++ src/ai_companion/lab/memory_manager.py
@@ -2,6 +2,8 @@ from typing import List, Optional
 
 from pydantic import BaseModel , Field
 from ai_companion.lab.vector_store import VectorStore
+from ai_companion import settings
+from langchain_groq import ChatGroq
 
 class MemoryAnalysis(BaseModel):
     is_important: bool = Field(..., description = "is the message important enough to be remembered?")
@@ -11,5 +13,4 @@ class MemoryAnalysis(BaseModel):
 class MemoryManager:
     def __init__(self):
         self.vector_store = VectorStore()
-        self.llm = settings.SMALL_TEXT_MODEL_NAME
-
+        self.llm = ChatGroq(model = settings.SMALL_TEXT_MODEL_NAME, api_key=settings.GROQ_API_KEY, temperature = 0.1, max_retries = 3).with_structured_output(MemoryAnalysis)
